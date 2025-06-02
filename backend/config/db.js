88222cfeace11dad9c4aa9e86b243c46cfb9ad1a
yahya4895 <yahyahussain1871@gmail.com>
2025-06-02 17:03:54 +0500

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
 
-    await mongoose.connect( mongoStr || process.env.MONGODB_URI, {
+    await mongoose.connect( process.env.MONGODB_URI, {
     });
     console.log('MongoDB connected');
   } catch (error) {
